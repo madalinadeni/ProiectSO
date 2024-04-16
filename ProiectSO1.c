7dfbe7ccc47b4cad7803d222d16f4511acdd174f
madalinadeni <madalina.negru@student.upt.ro>
2024-04-16 22:03:55 +0300

    Rezolvare cerinte 2 si 3

--- ProiectSO1.c
+++ ProiectSO1.c
@@ -1,3 +1,4 @@
+
 #include <stdlib.h>
 #include <stdio.h>
 #include <sys/types.h>
@@ -7,11 +8,13 @@
 #include <unistd.h>
 #include <sys/stat.h>
 #include <limits.h>
+#include <sys/wait.h>
+#include <time.h>
 
 struct Date {
     char nume_d[300];
-    long ultima_modificare;
-    long size;
+    char data_modificare[20];
+    off_t marime;
 };
 
 int verificare_existenta(const char *path) {
@@ -19,8 +22,11 @@ int verificare_existenta(const char *path) {
     return (stat(path, &buffer) == 0);
 }
 
-void actualizareSnapshot(const char *director) {
-    FILE *snapshotFile = fopen("snapshot.txt", "w");
+void actualizareSnapshot(const char *director, int nivel, const char *outputDir) {
+    char snapshotPath[PATH_MAX];
+    snprintf(snapshotPath, sizeof(snapshotPath), "%s/snapshot.txt", outputDir);
+    
+    FILE *snapshotFile = fopen(snapshotPath, "a");
     if (snapshotFile == NULL) {
         perror("Eroare la crearea fisierului de snapshot!");
         exit(1);
@@ -36,6 +42,12 @@ void actualizareSnapshot(const char *director) {
     while ((intrare = readdir(dir)) != NULL) {
         if (strcmp(intrare->d_name, ".") != 0 && strcmp(intrare->d_name, "..") != 0) {
             char filePath[PATH_MAX];
+            char linkPath[PATH_MAX+1], spatii[PATH_MAX];
+            int n;
+
+            memset(spatii, ' ', 2*nivel);
+            spatii[2*nivel]='\0';
+
             snprintf(filePath, sizeof(filePath), "%s/%s", director, intrare->d_name);
 
             struct stat fileInfo;
@@ -44,96 +56,108 @@ void actualizareSnapshot(const char *director) {
                 continue;
             }
 
-            fprintf(snapshotFile, "%s\t%ld\n", intrare->d_name, (long)fileInfo.st_mtime);
+            struct tm *tm_info;
+            char buffer[20];
+            tm_info = localtime(&fileInfo.st_mtime);
+            strftime(buffer, sizeof(buffer), "%Y-%m-%d %H:%M:%S", tm_info);
+
+            if(S_ISDIR(fileInfo.st_mode)){
+                actualizareSnapshot(filePath, nivel+1, outputDir);
+            }
+
+            if(S_ISLNK(fileInfo.st_mode)){
+                n=readlink(filePath, linkPath, sizeof(linkPath));
+                linkPath[n]='\0';
+                fprintf(snapshotFile, "%s\t%s -> %s\t%s\t%ld\n", intrare->d_name, filePath, linkPath, buffer, fileInfo.st_size);
+            }
+            else{
+                fprintf(snapshotFile, "%s\t%s\t%s\t%ld", intrare->d_name, filePath, buffer, fileInfo.st_size);
+                if(fileInfo.st_mode & S_IXUSR || fileInfo.st_mode & S_IXGRP || fileInfo.st_mode & S_IXOTH)
+                    fprintf(snapshotFile, "*");
+                fprintf(snapshotFile, "\n");
+            }
         }
     }
 
     closedir(dir);
     fclose(snapshotFile);
+    printf("Snapshot-ul pentru directorul %s a fost generat cu succes!\n", director);
 }
 
-int comparare_fisiere(const struct Date *fileInfo, const char *director) {
-    char filePath[PATH_MAX];
-    snprintf(filePath, sizeof(filePath), "%s/%s", director, fileInfo->nume_d);
+void childProcess(const char *director, int nivel, const char *outputDir) {
+    pid_t pid = getpid();
+    actualizareSnapshot(director, nivel, outputDir);
+    printf("Procesul cu PID-ul %d s-a incheiat cu codul 0\n", pid);
+    exit(0);
+}
 
-    struct stat currentInfo;
-    if (stat(filePath, &currentInfo) != 0) {
-        return 1;
+int main(int argc, char *argv[]) {
+    if (argc < 3 || argc > 12) {
+        fprintf(stderr, "Dati un numar de argumente cuprins intre 2 si 11");
+        exit(1);
     }
 
-    return (fileInfo->ultima_modificare != currentInfo.st_mtime);
-}
+    const char *outputDir = NULL;
+    int dirIndex = -1;
 
-void afisare_fisiere_modificate(const struct Date *fileInfo, int numarFisiere, const char *director) {
-    for (int i = 0; i < numarFisiere; ++i) {
-        if (comparare_fisiere(&fileInfo[i], director)) {
-            printf("%s a fost modificat!\n", fileInfo[i].nume_d);
+    for (int i = 1; i < argc; ++i) {
+        if (strcmp(argv[i], "-o") == 0) {
+            if (i + 1 < argc) {
+                outputDir = argv[i + 1];
+                dirIndex = i + 1;
+                break;
+            } else {
+                fprintf(stderr, "Optiunea -o necesita un argument valid!");
+                exit(1);
+            }
         }
     }
-}
 
-void incarcare_Snapshot(const char *director, struct Date **fileInfo, int *numarFisiere) {
-    FILE *snapshotFile = fopen("snapshot.txt", "r");
-    if (snapshotFile == NULL) {
-        perror("Eroare la deschiderea fisierului snapshot!");
+    if (outputDir == NULL) {
+        fprintf(stderr, "Lipseste directorul de iesire specificat prin optiunea -o!");
         exit(1);
     }
 
-    int capacitate = 11;
-    *fileInfo = malloc(capacitate * sizeof(struct Date));
-    if (*fileInfo == NULL) {
-        perror("Eroare la alocarea memoriei dinamice!");
-        exit(1);
-    }
+    for (int i = 1; i < argc; ++i) {
+        if (i == dirIndex) {
+            ++i;
+            continue;
+        }
 
-    *numarFisiere = 0;
-    char nume_d[PATH_MAX];
-    long int ultima_modificare;
-    while (fscanf(snapshotFile, "%s\t%ld", nume_d, &ultima_modificare) != EOF) {
-        if (*numarFisiere == capacitate) {
-            capacitate *= 2;
-            *fileInfo = realloc(*fileInfo, capacitate * sizeof(struct Date));
-            if (*fileInfo == NULL) {
-                perror("Realocarea memoriei a esuat!");
+        const char *director = argv[i];
+        if (!verificare_existenta(director)) {
+            fprintf(stderr, "Directorul %s nu exista!\n", director);
+            continue;
+        }
+
+        char outputPath[PATH_MAX];
+        snprintf(outputPath, sizeof(outputPath), "%s/%s", outputDir, director);
+
+        if (!verificare_existenta(outputPath)) {
+            if (mkdir(outputPath, 0777) == -1) {
+                perror("Eroare la crearea directorului de iesire!");
                 exit(1);
             }
         }
 
-        strcpy((*fileInfo)[*numarFisiere].nume_d, nume_d);
-        (*fileInfo)[*numarFisiere].ultima_modificare = ultima_modificare;
-        ++(*numarFisiere);
+        pid_t pid = fork();
+        if (pid == 0) {
+            childProcess(director, 1, outputPath);
+        } else if (pid < 0) {
+            perror("Eroare la crearea procesului copil!");
+            exit(1);
+        }
     }
 
-    fclose(snapshotFile);
-}
+    for (int i = 1; i < argc; ++i) {
+        if (i == dirIndex) {
+            ++i;
+            continue;
+        }
 
-int main(int argc, char *argv[]) {
-    if (argc < 2 || argc > 11) {
-        fprintf(stderr, "Dati un numar de argumente cuprins intre 1 si 10");
-        exit(1);
+        wait(NULL);
     }
 
-    const char *director = argv[1];
-    if (!verificare_existenta(director)) {
-        fprintf(stderr, "Directorul nu exista!");
-        exit(1);
-    }
-
-    if (!verificare_existenta("snapshot.txt")) {
-        printf("Snapshot-ul nu exista\n");
-        actualizareSnapshot(director);
-        printf("Snapshot-ul a fost creat.\n");
-    } else {
-        printf("Snapshot-ul exista!\n");
-    }
-
-    struct Date *fileInfo;
-    int numarFisiere;
-    incarcare_Snapshot(director, &fileInfo, &numarFisiere);
-
-    actualizareSnapshot(director);
-    afisare_fisiere_modificate(fileInfo, numarFisiere, director);
-
-    free(fileInfo);
     return 0;
 }
+
